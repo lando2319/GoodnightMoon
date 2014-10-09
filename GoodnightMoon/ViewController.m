8a64e9c0eb7f36e226edb8a17c027b37e6671d15
Mike Land <mikepland@gmail.com>
2014-10-09 14:46:26 -0500

    sets up gravity for bouncing

--- GoodnightMoon/ViewController.m
+++ GoodnightMoon/ViewController.m
@@ -36,8 +36,33 @@
 - (IBAction)handlePan:(UIPanGestureRecognizer *)gesture
 {
     CGPoint point = [gesture translationInView:gesture.view];
+    CGFloat yVelocity = [gesture velocityInView:gesture.view].y;
+
     self.shadeView.center = CGPointMake(self.shadeView.center.x, self.shadeView.center.y + point.y);
     [gesture setTranslation:CGPointMake(0, 0) inView:gesture.view];
+
+    if (gesture.state == UIGestureRecognizerStateEnded) {
+        [self.dynamicAnimator updateItemUsingCurrentState:self.shadeView];
+
+        if (yVelocity >= -500) {
+            [self.gravityBehavior setGravityDirection:CGVectorMake(0, -1)];
+            [self.dynamicItemBehavior setElasticity:0.5];
+            [self.pushBehavior setPushDirection:CGVectorMake(0, [gesture velocityInView:gesture.view].y)];
+        } else if (yVelocity >= -500 && yVelocity < 0) {
+            [self.gravityBehavior setGravityDirection:CGVectorMake(0, -1)];
+            [self.dynamicItemBehavior setElasticity:0.25];
+            [self.pushBehavior setPushDirection:CGVectorMake(0, [gesture velocityInView:gesture.view].y)];
+        } else if (yVelocity >= 0 && yVelocity < 500) {
+            [self.gravityBehavior setGravityDirection:CGVectorMake(0, -1)];
+            [self.dynamicItemBehavior setElasticity:0.25];
+            [self.pushBehavior setPushDirection:CGVectorMake(0, 500)];
+        } else {
+            [self.gravityBehavior setGravityDirection:CGVectorMake(0, 1)];
+            [self.dynamicItemBehavior setElasticity:0.5];
+            [self.pushBehavior setPushDirection:CGVectorMake(0, [gesture velocityInView:gesture.view].y)];
+        }
+
+    }
 }
 
 - (void)didReceiveMemoryWarning {
